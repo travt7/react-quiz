afe0cec987e163f001f44efd20d967ae9c0fff04
Travis Tingle <travt7@gmail.co>
2024-01-22 12:23:49 -0500

    create header component and main component and create their instances inside of App component JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
+//pretend we are loading the quiz questions from somewhere. creating a
+//fake API using an npm package called JSON server.
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,8 @@
+//simple presentational component that doesn't accept props
 function Header() {
   return (
-    <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
+    <header className="app-header">
+      <img src="logo512.png" alt="React logo" />
       <h1>The React Quiz</h1>
     </header>
   );
